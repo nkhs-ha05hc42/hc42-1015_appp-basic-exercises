1cf1e8ee36618b56a36302b71e8e9ea3527f500a
s_h421015 <nkhshc421015@g.neec.ac.jp>
2024-12-23 15:52:23 +0900

    6-4-1～6-4-5

---
+++ 6-4/6-4-1.mjs
@@ -0,0 +1,4 @@
+const score = 98
+if(score >= 90){
+    console.log("Excellent")
+}
---
+++ 6-4/6-4-4.mjs
@@ -0,0 +1,4 @@
+const value = 11
+const isEvenNumber = value % 3 === 0
+const result = isEvenNumber ? "3の倍数" : "それ以外"
+console.log(result)
